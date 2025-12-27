5222b9b267d16d64e1096732bb28a281c6725908
RadmilaGram <radmila.graham@gmail.com>
2025-12-27 21:29:00 +0200

    docs(swagger): add swagger documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,15 +5,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   const config = new DocumentBuilder()
-    .setTitle('API Documentation')
+    .setTitle('API')
     .setDescription('API description')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document); 
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(4000);
 }
